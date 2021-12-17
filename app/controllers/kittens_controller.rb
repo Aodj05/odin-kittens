ba2f7b74a2bbe6f18f2e2f9796b1f86b91c14d9e
Aodj05 <Aodj05@msn.com>
2021-12-17 11:54:47 -0500

    Initial Commit

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,68 @@
+class KittensController < ApplicationController
+    before_action :set_kitten, only: %i[ show edit update destroy ]
+
+    def index
+        @kittens = Kitten.all
+
+        respond_to do |format|
+            format.html 
+            format.json { render :json => @kittens }
+        end
+    end
+
+    def show
+        
+    end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def edit
+        
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params)
+
+        respond_to do |format|
+            if @kitten.save
+                format.html { redirect_to @kitten, notice: "Kitten saved successfully" }
+                format.json { render :show, status: :created, location: @kitten }
+            else 
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @kitten.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @kitten.update(kitten_params)
+                format.html { redirect_to @kitten, notice: "Kitten updated successfully"}
+                format.json { render :show, status: :ok, location: @kitten }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @kitten.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        @kitten.destroy
+        respond_to do |format|
+            format.html { redirect_to kittens_url, notice: "Kitten unsaved"}
+            format.json { head :no_content }
+        end
+    end
+
+    private
+
+    def set_kitten
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+end
